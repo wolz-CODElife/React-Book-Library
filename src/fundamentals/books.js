758a39b6863bc593359d425049b1478c9d8ccc1d
wolz-CODElife <joeladewole3@gmail.com>
2021-07-12 02:11:19 +0100

    Pushing to github

--- src/fundamentals/books.js
+++ src/fundamentals/books.js
@@ -1,23 +1,23 @@
 export const books = [
     {
         id : 1,
-        img : '',
-        title : 'I Love You to the Moon and Back',
-        author : 'Amelia Hepworth',
-        link : ''
+        img : 'https://images-na.ssl-images-amazon.com/images/I/5103HgmdtjS._AC_SX184_.jpg',
+        title : 'The Bad Guys in Cut to the Chase',
+        author : 'Aaron Blabey',
+        link : 'https://www.amazon.com/dp/1338329529/ref=s9_acsd_hps_bw_c2_x_1_i?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-8&pf_rd_r=KRR9N6YFRAHB41QXSHQ5&pf_rd_t=101&pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&pf_rd_i=283155'
     },
     {
         id : 2,
-        img : '',
-        title : 'Our Class is a Family',
-        author : 'Shannon Olsen',
-        link : ''
+        img : 'https://images-na.ssl-images-amazon.com/images/I/41uORm5aOQS._AC_SX184_.jpg',
+        title : 'One Last Stop',
+        author : 'Casey McQuiston',
+        link : 'https://www.amazon.com/dp/1250244498/ref=s9_acsd_hps_bw_c2_x_2_i?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-8&pf_rd_r=KRR9N6YFRAHB41QXSHQ5&pf_rd_t=101&pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&pf_rd_i=283155'
     },
     {
         id : 3,
-        img : '',
-        title : 'The Vanishimg Half: A Novel',
-        author : 'Brit Bennett',
-        link : ''
+        img : 'https://images-na.ssl-images-amazon.com/images/I/41HbeJH0GtL._AC_SX184_.jpg',
+        title : 'Ridgeline: A Novel',
+        author : 'Michael Punke',
+        link : 'https://www.amazon.com/dp/1250310466/ref=s9_acsd_hps_bw_c2_x_4_i?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=merchandised-search-8&pf_rd_r=KRR9N6YFRAHB41QXSHQ5&pf_rd_t=101&pf_rd_p=471146b1-73a8-45e2-aa6e-e79125421657&pf_rd_i=283155'
     },
 ]
